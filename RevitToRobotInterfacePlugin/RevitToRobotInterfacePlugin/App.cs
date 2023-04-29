de1abcb98f41cf10ab5810ccaba46d8101f65961
lsbudisso23 <132138206+lsbudisso23@users.noreply.github.com>
2023-04-29 19:00:17 +0100

    Update App.cs
    Updated to post the robot structural analysis command instead of the print preview command.
    
    Unfortunately this does not work as intended.
    No exceptions are caused and the cmd id for this string is successfully resolved (not null),
    however upon investigation, revit does not appear to understand this command as the following
    entry is written to the current journal:
       'C 01-Mar-2023 15:25:19.693;  DBG_INFO: CustomCtrl_%CustomCtrl_%CustomCtrl_%Analyze%Structural Analysis%AnalysisAndCodeCheck%RobotStructuralAnalysisLinkdoes not exist.: line 894 of E:\Ship\2022_px64\Source\API\RevitAPIUI\Objects\APIUIApplicationHandwritten.cpp.
    likewise with the python version, using PostCommand appears to be a dead end...

--- RevitToRobotInterfacePlugin/RevitToRobotInterfacePlugin/App.cs
+++ RevitToRobotInterfacePlugin/RevitToRobotInterfacePlugin/App.cs
@@ -25,13 +25,28 @@ namespace RevitToRobotInterfacePlugin
                 {
                     tx.Start("Posting command from Handler");
 
-                    // get revit cmd id for print preview command
-                    // print preview us used as PostCommand does not return anything that can be used to determin the 
-                    // success of the command, and so an visually obvious command (print preview) is used for testing
-                    // as causes a change to the active screen
-                    RevitCommandId cmd = RevitCommandId.LookupPostableCommandId(PostableCommand.PrintPreview);
+                    // get revit cmd id for robot structural analysis 
+                    // this command string is obtained upon issuing the command manually (via the gui) in revit
+                    // and observing the external command printed in the journal file for the active session
+                    // (C:\Users\<username>\AppData\Local\Autodesk\Revit\Autodesk Revit 2022\Journals) as stated in
+                    // https://help.autodesk.com/view/RVT/2022/ENU/?guid=Revit_API_Revit_API_Developers_Guide_Advanced_Topics_Commands_html
+                    // The journal entry is as follows:
+                    //   ' [Jrn.Tooltip] Rvt.Attr.Tooltip.CommandID: CustomCtrl_%CustomCtrl_%CustomCtrl_%Analyze%Structural Analysis%AnalysisAndCodeCheck%RobotStructuralAnalysisLink Rvt.Attr.Tooltip.ElapsedTime: 0.1351142 Rvt.Attr.Tooltip.Enabled: True Rvt.Attr.Tooltip.IsFirst: True Rvt.Attr.Tooltip.Key: CustomCtrl_%CustomCtrl_%CustomCtrl_%Analyze%Structural Analysis%AnalysisAndCodeCheck%RobotStructuralAnalysisLink Rvt.Attr.Tooltip.Progressive: True Rvt.Attr.Tooltip.Progressive.Delay: 2 
+                    //   'E 28-Feb-2023 20:46:47.151;   0:< 
+                    //   Jrn.RibbonEvent "Execute external command:CustomCtrl_%CustomCtrl_%CustomCtrl_%Analyze%Structural Analysis%AnalysisAndCodeCheck%RobotStructuralAnalysisLink:REX.DRevit2Robot.DirectRevitAccess" 
+                    string s = "CustomCtrl_%CustomCtrl_%CustomCtrl_%Analyze%Structural Analysis%AnalysisAndCodeCheck%RobotStructuralAnalysisLink";
+                    RevitCommandId cmd = RevitCommandId.LookupCommandId(s);
 
-                    // post the print preview command
+                    // fixme
+                    // unfortunately this does not work as intended
+                    // no exceptions are caused, and the cmd id for this string is successfully resolved (not null),
+                    // however upon investigation, revit does not appear to understand this command as the following 
+                    // entry is written to the current journal:
+                    //   'C 01-Mar-2023 15:25:19.693;  DBG_INFO: CustomCtrl_%CustomCtrl_%CustomCtrl_%Analyze%Structural Analysis%AnalysisAndCodeCheck%RobotStructuralAnalysisLinkdoes not exist.: line 894 of E:\Ship\2022_px64\Source\API\RevitAPIUI\Objects\APIUIApplicationHandwritten.cpp. 
+                    // likewise with the python version, using PostCommand appears to be a dead end...
+
+
+                    // post the robot structural analysis command
                     uiapp.PostCommand(cmd);
 
                     // complete the transaction
@@ -44,17 +59,17 @@ namespace RevitToRobotInterfacePlugin
             // to the user for debugging
             catch (Autodesk.Revit.Exceptions.ArgumentNullException ex)
             {
-                Debug.Print($"error: Handler() RevitCommandId for PrintPreview could not be determined");
+                Debug.Print($"error: Handler() RevitCommandId for Robot Structural Analysis could not be determined");
                 Debug.Print(ex.Message);
             }
             catch (Autodesk.Revit.Exceptions.ArgumentException ex)
             {
-                Debug.Print($"error: Handler() RevitCommandId for PrintPreview could not be posted");
+                Debug.Print($"error: Handler() RevitCommandId for Robot Structural Analysis could not be posted");
                 Debug.Print(ex.Message);
             }
             catch (Autodesk.Revit.Exceptions.InvalidOperationException ex)
             {
-                Debug.Print($"error: Handler() RevitCommandId for PrintPreview could not be posted. This is likely as a result of multiple commands being posted (is the same event posting multiple commands?");
+                Debug.Print($"error: Handler() RevitCommandId for Robot Structural Analysis could not be posted. This is likely as a result of multiple commands being posted (is the same event posting multiple commands?");
                 Debug.Print(ex.Message);
             }
             // handle any other exception by printing to the debug console
